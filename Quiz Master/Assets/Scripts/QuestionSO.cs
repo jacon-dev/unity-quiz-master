4271b5a7412d21f4826a912c93cf9fb9041bf22c
James Conlin <jacon.dev@outlook.com>
2023-09-27 18:59:44 +0100

    adding logic to facilitate a wrong answer and highlight correct one

--- Quiz Master/Assets/Scripts/QuestionSO.cs
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -5,9 +5,7 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
-
     [SerializeField] string[] answers = new string[4];
-
     [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
--- Quiz Master/Assets/Scripts/Quiz.cs
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -1,14 +1,38 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
-
     [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+    int correctAnswerIndex;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            var buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if(index == correctAnswerIndex)
+        {
+            questionText.text = "Well done! That is correct!";
+            answerButtons[index].GetComponent<Image>().sprite = correctAnswerSprite;
+        }
+        else
+        {
+            questionText.text = $"Woops! Sorry, the correct answer was {question.GetAnswer(correctAnswerIndex)}";
+            answerButtons[correctAnswerIndex].GetComponent<Image>().sprite = correctAnswerSprite;
+        }
     }
 }
