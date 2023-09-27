6f5367ae99cd86b72923a3bcbbcd80f63b47e334
James Conlin <jacon.dev@outlook.com>
2023-09-27 18:03:57 +0100

    adding questions and logic to allow us to get question text onto the ui

---
+++ Quiz Master/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "NewQuestion")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new question text here";
+
+    [SerializeField] string[] answers = new string[4];
+
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int answerIndex)
+    {
+        return answers[answerIndex];
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+}
---
+++ Quiz Master/Assets/Scripts/Quiz.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] QuestionSO question;
+
+    [SerializeField] TextMeshProUGUI questionText;
+
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+    }
+}
